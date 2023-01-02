d36ce4487ca8e457890be60fc92c25c562a970a0
Aryan Singh <96579866+AryanSinghIIIT@users.noreply.github.com>
2023-01-02 19:11:46 +0530

    Add files via upload

---
+++ components/index.js
@@ -0,0 +1,13 @@
+export { default as Footer } from './Footer';
+export { default as Layout } from './Layout';
+export { default as Navbar } from './Navbar';
+export { default as Product } from './Product';
+export { default as HeroBanner } from './HeroBanner';
+export { default as FooterBanner } from './FooterBanner';
+export { default as Cart } from './Cart';
+
+// NEXT_PUBLIC_SANITY_TOKEN =
+// skqtJeam2KEaPUFPoQdFXqMBTqdhQBO0fOHq91AlTyLUlVJGXpNFXdYvGRy34h3PbOxgSXgFW8or62ehWfB1EIxa85KUlwGBi9xzO5UlPehNUjtSNFQAtIqiVteFvfgMM7jCY5m5LdWxW832ZijIY0NJECh8TDSyfZtEzXwNbMaUruBOKiQZ
+
+// NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY=pk_test_51L0R9GSDiUu0rdhWI1U3l2qKkvjNL63sYslGnDuJ03CXkxrivmLOpPT810qY61mHfcfNjvNuvvpy1twNdwCP4BYj00act2hf1A
+// STRIPE_SECRET_KEY=sk_test_51L0R9GSDiUu0rdhWKsYFxSsiaZKgBKKKfeqKePFnPAnAkWOmBBraNnzgwfBj4jgrhAly123HHrb0KUsxwNNG7GPS005TjNwO9A
